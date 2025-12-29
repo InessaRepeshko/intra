4ce6e1c1ccd199b5d37e337dfe838f6379493d75
Inessa Repeshko <inessa.repeshko92@gmail.com>
2025-12-29 19:03:24 +0200

    Refactor module paths to align with DDD structure in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './models/users/users.module';
-import { Feedback360Module } from './models/feedback360/feedback360.module';
-import { TeamsModule } from './models/teams/teams.module';
+import { UsersModule } from './modules/users/infrastructure/users.module';
+import { Feedback360Module } from './modules/feedback360/infrastructure/feedback360.module';
+import { TeamsModule } from './modules/teams/infrastructure/teams.module';
 
 @Module({
   imports: [UsersModule, Feedback360Module, TeamsModule],
